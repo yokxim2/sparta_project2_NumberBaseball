dd5722251828e16668e3708456bc58f0ae753ed7
yokxim <yooksi53@gmail.com>
2024-09-12 17:56:09 +0900

    initialize GraderLv1

--- src/grade/GraderLv1.java
+++ src/grade/GraderLv1.java
@@ -3,12 +3,50 @@ package grade;
 import generate.Generator;
 import generate.GeneratorLv1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GraderLv1 implements Grader {
 
-    Generator generator = new GeneratorLv1();
+    private final int answer = (new GeneratorLv1()).generate();
+    private int strikeCount = 0;
+    private int ballCount = 0;
 
     @Override
     public int grade(int number) {
-        int answer = generator.generate();
+        ArrayList<Integer> answerToList = numberToList(answer);
+        ArrayList<Integer> targetNumberToList = numberToList(number);
+        for (int i = 0; i < targetNumberToList.size(); i++) {
+            int targetNumber = targetNumberToList.get(i);
+            if (answerToList.contains(targetNumber)) {
+                if (answerToList.get(i) == targetNumber) {
+                    strikeCount++;
+                } else {
+                    ballCount++;
+                }
+            }
+        }
+        printResult();
+        return strikeCount;
+    }
+
+    private ArrayList<Integer> numberToList(int number) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        while (number > 0) {
+            list.add(0, number % 10);
+            number /= 10;
+        }
+        return list;
+    }
+
+    private void printResult() {
+        if (strikeCount == 3) {
+            System.out.println("정답입니다!");
+        } else if (strikeCount == 0 && ballCount == 0) {
+            System.out.println("아웃!");
+        } else {
+            System.out.println(strikeCount + " 스트라이크 " + ballCount + " 볼!");
+        }
     }
 }
