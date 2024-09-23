8841daa118a3fbdca2fa7a7dfdbf98a56aaa131a
yokxim <yooksi53@gmail.com>
2024-09-23 16:36:05 +0900

    small comment on enum class

--- src/config/AllowedNumberDigit.java
+++ src/config/AllowedNumberDigit.java
@@ -15,6 +15,7 @@ public enum AllowedNumberDigit {
         return numberDigit;
     }
 
+    // 자리수가 허가된 범위 내에 있는지 조사하고, 일치하지 않을 시 Null을 반환하도록 있도록 했다. (기본형 int를 반환형으로 하지 않은 이유)
     public static AllowedNumberDigit contains(int numberDigit) {
         for (AllowedNumberDigit value : AllowedNumberDigit.values()) {
             if (value.getNumberDigit() == numberDigit) {
